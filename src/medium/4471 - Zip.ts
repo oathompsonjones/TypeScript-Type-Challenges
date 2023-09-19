d534ebc269797a8a307ebbcf63cdcdbf37a6e087
Ollie Jones <oathompsonjones@gmail.com>
2023-09-19 12:40:18 +0100

    Zip

---
+++ src/medium/4471 - Zip.ts
@@ -0,0 +1,40 @@
+/*
+  4471 - Zip
+  -------
+  by キリサメ qianxi (@qianxi0410) #medium #tuple
+
+  ### Question
+
+  In This Challenge, You should implement a type `Zip<T, U>`, T and U must be `Tuple`
+  ```ts
+  type exp = Zip<[1, 2], [true, false]> // expected to be [[1, true], [2, false]]
+  ```
+
+  > View on GitHub: https://tsch.js.org/4471
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Zip<T extends unknown[], U extends unknown[]> = T extends [infer T1, ...infer T2]
+    ? U extends [infer U1, ...infer U2]
+        ? [[T1, U1], ...Zip<T2, U2>]
+        : []
+    : [];
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+export type Cases = [
+    Expect<Equal<Zip<[], []>, []>>,
+    Expect<Equal<Zip<[1, 2], [true, false]>, [[1, true], [2, false]]>>,
+    Expect<Equal<Zip<[1, 2, 3], ["1", "2"]>, [[1, "1"], [2, "2"]]>>,
+    Expect<Equal<Zip<[], [1, 2, 3]>, []>>,
+    Expect<Equal<Zip<[[1, 2]], [3]>, [[[1, 2], 3]]>>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/4471/answer
+  > View solutions: https://tsch.js.org/4471/solutions
+  > More Challenges: https://tsch.js.org
+*/
